2cbc58da66213534c77f21e4f80ac562c007ab1f
何永能 <yongneng.he@dianping.com>
2017-07-13 16:10:02 +0800

    crud

--- app-plant-dao/src/main/java/com/nengjun/app/plant/dao/mapper/PoiPlantMapper.java
+++ app-plant-dao/src/main/java/com/nengjun/app/plant/dao/mapper/PoiPlantMapper.java
@@ -1,15 +1,28 @@
 package com.nengjun.app.plant.dao.mapper;
 
 import com.nengjun.app.plant.dao.entity.PoiPlant;
-import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.*;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 /**
  * Created by Henry on 2017/7/13.
  */
 @Mapper
 public interface PoiPlantMapper {
+    @Select("select * from poi_plant")
+    List<PoiPlant> selectList();
+
     @Select("select * from poi_plant where id = #{id}")
-    PoiPlant findById(@Param("id") Integer id);
+    PoiPlant selectByPrimaryKey(Integer id);
+
+    @Insert("insert into poi_plant (name) value (#{record.name})")
+    int insert(@Param("record") PoiPlant record);
+
+    @Update("update poi_plant set name = #{record.name} where id = #{record.id}")
+    int updateByPrimaryKey(@Param("record") PoiPlant record);
+
+    @Delete("delete from poi_plant where id = #{id}")
+    int deleteByPrimaryKey(Integer id);
 }
--- app-plant-web/src/test/java/com/nengjun/app/plant/dao/mapper/PoiPlantMapperTest.java
+++ app-plant-web/src/test/java/com/nengjun/app/plant/dao/mapper/PoiPlantMapperTest.java
@@ -15,7 +15,21 @@ public class PoiPlantMapperTest extends AbstractTest {
 
     @Test
     public void test1() {
-        PoiPlant plant = poiPlantMapper.findById(1);
-        Assert.assertEquals("玫瑰花", plant.getName());
+        // 测试新增
+        PoiPlant plant = new PoiPlant();
+        plant.setName("栀子花");
+        poiPlantMapper.insert(plant);
+        plant = poiPlantMapper.selectByPrimaryKey(2);
+        Assert.assertEquals("栀子花", plant.getName());
+
+        // 测试更新
+        plant.setName("月季");
+        poiPlantMapper.updateByPrimaryKey(plant);
+        plant = poiPlantMapper.selectByPrimaryKey(2);
+        Assert.assertEquals("月季", plant.getName());
+
+        // 测试删除
+        poiPlantMapper.deleteByPrimaryKey(2);
+        Assert.assertEquals(1, poiPlantMapper.selectList().size());
     }
 }
