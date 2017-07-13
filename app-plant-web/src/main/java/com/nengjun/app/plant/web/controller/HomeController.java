497b73e62c881cb646ffce51f378f55baae90a0d
何永能 <yongneng.he@dianping.com>
2017-07-13 13:09:53 +0800

    logging

--- app-plant-web/src/main/java/com/nengjun/app/plant/web/controller/HomeController.java
+++ app-plant-web/src/main/java/com/nengjun/app/plant/web/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.nengjun.app.plant.web.controller;
 import com.nengjun.app.plant.web.config.GlobalSetting;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,5 +17,10 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         return String.format("env: %s", globalSetting.getEnv());
+    }
+
+    @GetMapping("/say")
+    public String say(@RequestParam(value = "id", required = false, defaultValue = "0") Integer myId) {
+        return String.format("id: %d", myId);
     }
 }
