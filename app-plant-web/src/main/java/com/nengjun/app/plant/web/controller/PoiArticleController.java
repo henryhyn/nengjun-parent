2dd4959f79803f384fa730ae7c746b96309add4c
何永能 <yongneng.he@dianping.com>
2017-09-12 13:05:58 +0800

    md

--- app-plant-web/src/main/java/com/nengjun/app/plant/web/controller/PoiArticleController.java
+++ app-plant-web/src/main/java/com/nengjun/app/plant/web/controller/PoiArticleController.java
@@ -2,26 +2,16 @@ package com.nengjun.app.plant.web.controller;
 
 import com.nengjun.app.content.dao.entity.PoiArticle;
 import com.nengjun.app.content.dao.mapper.PoiArticleMapper;
-import com.nengjun.app.shop.dao.entity.PoiTag;
-import com.nengjun.app.shop.dao.mapper.PoiTagMapper;
 import com.nengjun.avatar.face.type.PageModel;
 import com.nengjun.avatar.face.type.Result;
 import com.nengjun.avatar.face.utils.ResultUtil;
 import com.nengjun.avatar.face.utils.Validate;
-import org.apache.commons.lang3.math.NumberUtils;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by Henry on 2017/7/19.
@@ -58,7 +48,8 @@ public class PoiArticleController {
         PoiArticle article = new PoiArticle();
         copyProperties(poiArticle, article);
         article.setStatus(0);
-        return ResultUtil.success(poiArticleMapper.insert(article));
+        poiArticleMapper.insert(article);
+        return ResultUtil.success(article);
     }
 
     @PutMapping("/articles/{id}")
@@ -70,7 +61,8 @@ public class PoiArticleController {
         PoiArticle article = poiArticleMapper.selectByPrimaryKey(id);
         Validate.hasRecord("id", id, article);
         copyProperties(poiArticle, article);
-        return ResultUtil.success(poiArticleMapper.updateByPrimaryKey(article));
+        poiArticleMapper.updateByPrimaryKey(article);
+        return ResultUtil.success(article);
     }
 
     @DeleteMapping("/articles/{id}")
--- avatar-mybatis/src/main/java/com/nengjun/avatar/mybatis/mapper/base/insert/InsertMapper.java
+++ avatar-mybatis/src/main/java/com/nengjun/avatar/mybatis/mapper/base/insert/InsertMapper.java
@@ -2,11 +2,13 @@ package com.nengjun.avatar.mybatis.mapper.base.insert;
 
 import com.nengjun.avatar.mybatis.provider.base.BaseInsertProvider;
 import org.apache.ibatis.annotations.InsertProvider;
+import org.apache.ibatis.annotations.SelectKey;
 
 /**
  * Created by Henry on 2017/7/13.
  */
 public interface InsertMapper<T, PK> {
+    @SelectKey(statement = "SELECT LAST_INSERT_ID()", keyProperty = "id", before = false, resultType = Integer.class)
     @InsertProvider(type = BaseInsertProvider.class, method = "dynamicSQL")
     int insert(T record);
 }
