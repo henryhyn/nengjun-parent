3361de8fb79a123f8054fd170b9d61cdcbfc387a
何永能 <yongneng.he@dianping.com>
2017-09-13 15:36:26 +0800

    fix

--- avatar-mybatis/src/main/java/com/nengjun/avatar/mybatis/helper/PageInterceptor.java
+++ avatar-mybatis/src/main/java/com/nengjun/avatar/mybatis/helper/PageInterceptor.java
@@ -15,10 +15,7 @@ import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.RowBounds;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 
 /**
  * Created by Henry on 2017/7/18.
@@ -43,16 +40,36 @@ public class PageInterceptor implements Interceptor {
 
         PageModel<?> pageModel = (PageModel<?>) parameter;
         Executor executor = (Executor) invocation.getTarget();
-        BoundSql boundSql = ms.getBoundSql(parameter);
+        BoundSql boundSql = ms.getBoundSql(null);
+
+        BoundSql newBoundSql = getWhereString(ms, boundSql, pageModel.getConditions());
 
         // 计算总的行数
-        MappedStatement countMs = getCountMs(ms, boundSql);
-        Long total = executeCount(executor, countMs, resultHandler);
+        MappedStatement countMs = getCountMs(ms, newBoundSql);
+        Long total = executeCount(executor, countMs, newBoundSql.getParameterObject(), resultHandler);
         pageModel.setTotal(total);
 
         // 生成分页 SQL 语句
-        args[MAPPED_STATEMENT_INDEX] = getPageMs(ms, boundSql, pageModel.getOffset(), pageModel.getPageSize());
+        args[MAPPED_STATEMENT_INDEX] = getPageMs(ms, newBoundSql, pageModel.getOffset(), pageModel.getPageSize());
+        args[PARAMETER_INDEX] = newBoundSql.getParameterObject();
         return invocation.proceed();
+    }
+
+    private BoundSql getWhereString(MappedStatement ms, BoundSql boundSql, Map<String, Object> conditions) {
+        if (conditions.isEmpty()) {
+            return boundSql;
+        }
+
+        List<ParameterMapping> parameterMappings = new ArrayList<>();
+        StringBuilder sb = new StringBuilder(boundSql.getSql());
+        sb.append(" where");
+        for (Map.Entry<String, Object> entry : conditions.entrySet()) {
+            sb.append(String.format(" (%s) and", entry.getKey()));
+            ParameterMapping parameterMapping = new ParameterMapping.Builder(ms.getConfiguration(), entry.getKey(), Object.class).build();
+            parameterMappings.add(parameterMapping);
+        }
+        sb.delete(sb.length() - 3, sb.length());
+        return new BoundSql(ms.getConfiguration(), sb.toString(), parameterMappings, conditions);
     }
 
     private MappedStatement getPageMs(MappedStatement ms, BoundSql boundSql, Integer offset, Integer pageSize) {
@@ -67,7 +84,8 @@ public class PageInterceptor implements Interceptor {
         PlainSelect countBody = ((PlainSelect) select.getSelectBody());
         countBody.setSelectItems(Collections.singletonList(new SelectExpressionItem(new Column("id"))));
         select.setSelectBody(countBody);
-        String pageSql = String.format("%s inner join (%s limit %d, %d) b where a.id = b.id", sql, select, offset, pageSize);
+//        String pageSql = String.format("%s inner join (%s limit %d, %d) b where a.id = b.id", sql, select, offset, pageSize);
+        String pageSql = String.format("%s limit %d, %d", sql, offset, pageSize);
         BoundSql newBoundSql = new BoundSql(ms.getConfiguration(), pageSql, boundSql.getParameterMappings(), boundSql.getParameterObject());
         MappedStatement.Builder builder = new MappedStatement.Builder(ms.getConfiguration(), ms.getId(), new BoundSqlSqlSource(newBoundSql), ms.getSqlCommandType());
         builder.resultMaps(ms.getResultMaps());
@@ -97,10 +115,10 @@ public class PageInterceptor implements Interceptor {
         return builder.build();
     }
 
-    private Long executeCount(Executor executor, MappedStatement countMs, ResultHandler resultHandler) {
+    private Long executeCount(Executor executor, MappedStatement countMs, Object parameter, ResultHandler resultHandler) {
         Object countResultList = null;
         try {
-            countResultList = executor.query(countMs, null, RowBounds.DEFAULT, resultHandler);
+            countResultList = executor.query(countMs, parameter, RowBounds.DEFAULT, resultHandler);
         } catch (SQLException e) {
             e.printStackTrace();
         }
