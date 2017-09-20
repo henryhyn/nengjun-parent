68b16771181d4496d3f97414ac927b756ea7dd7b
何永能 <yongneng.he@dianping.com>
2017-09-20 21:31:30 +0800

    space

--- app-plant-web/src/main/java/com/nengjun/app/plant/web/enums/BizCode.java
+++ app-plant-web/src/main/java/com/nengjun/app/plant/web/enums/BizCode.java
@@ -6,10 +6,18 @@ import lombok.Getter;
  * Created by Henry on 2017/9/19.
  */
 public enum BizCode {
-    SHOP(1, "http://owkrv24pf.bkt.clouddn.com/", "http://owkrm59it.bkt.clouddn.com/"), ACTIVITY(2, "http://owkr7o2r5.bkt.clouddn.com/", "http://owkr4viuk.bkt.clouddn.com/"), ARTICLE(3, "http://owkulzm7p.bkt.clouddn.com/", "http://owkui4i5d.bkt.clouddn.com/");
+    SHOP(0, "shop", "http://owkrv24pf.bkt.clouddn.com/", "http://owkrm59it.bkt.clouddn.com/")
+    , SHOP_REVIEW(1, "shop", "http://owkrv24pf.bkt.clouddn.com/", "http://owkrm59it.bkt.clouddn.com/")
+    , ACTIVITY(10, "activity", "http://owkr7o2r5.bkt.clouddn.com/", "http://owkr4viuk.bkt.clouddn.com/")
+    , ACTIVITY_REVIEW(11, "activity", "http://owkr7o2r5.bkt.clouddn.com/", "http://owkr4viuk.bkt.clouddn.com/")
+    , ARTICLE(20, "article", "http://owkulzm7p.bkt.clouddn.com/", "http://owkui4i5d.bkt.clouddn.com/")
+    , ARTICLE_REVIEW(21, "article", "http://owkulzm7p.bkt.clouddn.com/", "http://owkui4i5d.bkt.clouddn.com/");
 
     @Getter
     private int type;
+
+    @Getter
+    private String space;
 
     @Getter
     private String dev;
@@ -17,20 +25,27 @@ public enum BizCode {
     @Getter
     private String prod;
 
-    BizCode(int type, String dev, String prod) {
+    BizCode(int type, String space, String dev, String prod) {
         this.type = type;
+        this.space = space;
         this.dev = dev;
         this.prod = prod;
     }
 
     public static BizCode valueOf(int type) {
         switch (type) {
-            case 1:
+            case 0:
                 return SHOP;
-            case 2:
+            case 1:
+                return SHOP_REVIEW;
+            case 10:
                 return ACTIVITY;
-            case 3:
+            case 11:
+                return ACTIVITY_REVIEW;
+            case 20:
                 return ARTICLE;
+            case 21:
+                return ARTICLE_REVIEW;
             default:
                 return null;
         }
