cf8eb2df9cfd3d64be256fe8c63259285366234e
何永能 <yongneng.he@dianping.com>
2017-09-22 16:23:27 +0800

    httpclient

--- app-plant-web/src/main/java/com/nengjun/app/plant/web/config/HttpClientConfiguration.java
+++ app-plant-web/src/main/java/com/nengjun/app/plant/web/config/HttpClientConfiguration.java
@@ -1,16 +1,31 @@
 package com.nengjun.app.plant.web.config;
 
+import org.apache.commons.httpclient.DefaultHttpMethodRetryHandler;
 import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.MultiThreadedHttpConnectionManager;
+import org.apache.commons.httpclient.params.HttpConnectionManagerParams;
+import org.apache.commons.httpclient.params.HttpMethodParams;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
+ * 调优, 参考 http://hc.apache.org/httpclient-3.x/performance.html
+ * http://jsczxy2.iteye.com/blog/2117388
  * Created by Henry on 2017/9/16.
  */
 @Configuration
 public class HttpClientConfiguration {
     @Bean
     public HttpClient httpClient() {
-        return new HttpClient();
+        MultiThreadedHttpConnectionManager manager = new MultiThreadedHttpConnectionManager();
+        HttpClient client = new HttpClient(manager);
+        HttpConnectionManagerParams params = client.getHttpConnectionManager().getParams();
+        params.setDefaultMaxConnectionsPerHost(8); // 默认是 2
+        params.setMaxTotalConnections(48); // 默认是 20
+        params.setConnectionTimeout(5000); // 默认是 0
+        params.setSoTimeout(5000); // 默认是 0
+        params.setLinger(1000); // 默认是 -1
+        params.setParameter(HttpMethodParams.RETRY_HANDLER, new DefaultHttpMethodRetryHandler()); // 失败 3 次重试
+        return client;
     }
 }
