9dc4ef99e1ef14986e1b048b74ffebdd6e103054
何永能 <yongneng.he@dianping.com>
2017-09-25 19:52:34 +0800

    review

--- app-content-dao/src/main/java/com/nengjun/app/content/dao/mapper/PoiPictureMapper.java
+++ app-content-dao/src/main/java/com/nengjun/app/content/dao/mapper/PoiPictureMapper.java
@@ -3,10 +3,14 @@ package com.nengjun.app.content.dao.mapper;
 import com.nengjun.app.content.dao.entity.PoiPicture;
 import com.nengjun.avatar.mybatis.mapper.SqlMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * Created by Henry on 2017/7/19.
  */
 @Mapper
 public interface PoiPictureMapper extends SqlMapper<PoiPicture, Integer> {
+    List<PoiPicture> selectByBizIdAndRefIds(@Param("bizId") Integer bizId, @Param("list") List<Integer> refIds);
 }
--- app-plant-web/src/main/java/com/nengjun/app/plant/web/controller/PoiReviewController.java
+++ app-plant-web/src/main/java/com/nengjun/app/plant/web/controller/PoiReviewController.java
@@ -1,20 +1,27 @@
 package com.nengjun.app.plant.web.controller;
 
+import com.nengjun.app.content.dao.entity.PoiPicture;
+import com.nengjun.app.content.dao.mapper.PoiPictureMapper;
+import com.nengjun.app.plant.web.config.GlobalSetting;
 import com.nengjun.app.plant.web.enums.BizCode;
 import com.nengjun.app.user.dao.entity.PoiReview;
 import com.nengjun.app.user.dao.mapper.PoiReviewMapper;
+import com.nengjun.avatar.face.type.PageModel;
 import com.nengjun.avatar.face.type.Result;
 import com.nengjun.avatar.face.utils.ResultUtil;
 import com.nengjun.avatar.face.utils.Validate;
 import lombok.Data;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Created by Henry on 2017/9/16.
@@ -23,7 +30,53 @@ import javax.validation.Valid;
 @RequestMapping("/api")
 public class PoiReviewController {
     @Autowired
+    private GlobalSetting globalSetting;
+
+    @Autowired
     private PoiReviewMapper poiReviewMapper;
+
+    @Autowired
+    private PoiPictureMapper poiPictureMapper;
+
+    @GetMapping("/reviews")
+    public Result _index(
+            @RequestParam(value = "biz", required = true) String biz,
+            @RequestParam(value = "refId", required = true) Integer refId,
+            @RequestParam(value = "page", required = false, defaultValue = "1") Integer page,
+            @RequestParam(value = "pageSize", required = false, defaultValue = "20") Integer pageSize
+    ) {
+        PageModel<PoiReviewDTO> reviewPageModel = new PageModel<>();
+        reviewPageModel.setPageAndPageSize(page, pageSize);
+        BizCode bizCode = BizCode.valueOf(biz);
+        Integer bizId = bizCode.getType();
+        reviewPageModel.addCondition("biz_id = ?", bizId);
+        reviewPageModel.addCondition("ref_id = ?", refId);
+        List<PoiReview> reviewList = poiReviewMapper.selectByPage(reviewPageModel);
+        Validate.isEmpty("reviewList", reviewList);
+
+        List<Integer> reviewIds = reviewList.stream().map(PoiReview::getId).collect(Collectors.toList());
+        List<PoiPicture> pictureList = poiPictureMapper.selectByBizIdAndRefIds(bizId + 1, reviewIds);
+        Map<Integer, List<String>> picturesMap = new HashMap<>();
+        for (PoiPicture picture : pictureList) {
+            Integer key = picture.getRefId();
+            List<String> pictures = picturesMap.get(key);
+            if (pictures == null) {
+                picturesMap.put(key, new ArrayList<>());
+            }
+            picturesMap.get(key).add(getAbsolutePath(picture.getPictureKey(), bizId));
+        }
+
+        List<PoiReviewDTO> reviewDTOList = new ArrayList<>();
+        for (PoiReview review : reviewList) {
+            PoiReviewDTO reviewDTO = new PoiReviewDTO();
+            BeanUtils.copyProperties(review, reviewDTO);
+            reviewDTO.setPictures(picturesMap.get(review.getId()));
+            reviewDTOList.add(reviewDTO);
+        }
+
+        reviewPageModel.setList(reviewDTOList);
+        return ResultUtil.success(reviewPageModel);
+    }
 
     @PostMapping("/reviews")
     public Result create(@Valid @RequestBody PoiReviewDTO poiReview, BindingResult bindingResult) {
@@ -44,8 +97,18 @@ public class PoiReviewController {
         review.setReviewBody(poiReview.getReviewBody());
     }
 
+    private String getAbsolutePath(String key, int bizId) {
+        BizCode bizCode = BizCode.valueOf(bizId);
+        if (bizCode == null) {
+            return null;
+        }
+        String domain = "prod".equals(globalSetting.getEnv()) ? bizCode.getSpace().getProd() : bizCode.getSpace().getDev();
+        return domain + key;
+    }
+
     @Data
     private static class PoiReviewDTO extends PoiReview {
         private String biz;
+        private List<String> pictures;
     }
 }
